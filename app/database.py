a32f9c56ac5700ce457a9659793edadaa6de4031
mndrake95 <mandra4a@gmail.com>
2025-12-09 17:13:46 +0500

    modified:   app/database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from app.config import settings  # <--- Импорт настроек
+from app.config import settings  
 
 engine = create_async_engine(settings.DB_URL, echo=True)
 
